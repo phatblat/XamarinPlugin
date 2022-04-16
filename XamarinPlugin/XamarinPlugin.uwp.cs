fdfbe2378f65caae3c3bbd1dd26a1a97730b0140
Ben Chatelain <ben@octop.ad>
2022-04-16 16:04:14 -0600

    Add project files.

---
+++ XamarinPlugin/XamarinPlugin.uwp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.XamarinPlugin
+{
+    /// <summary>
+    /// Interface for XamarinPlugin
+    /// </summary>
+    public class XamarinPluginImplementation : IXamarinPlugin
+    {
+    }
+}
